3f6b34ed94e92756fb2626ac9bf763e408c9ba00
Luiz Otávio <Luiz.fontes0@outlook.com>
2026-05-27 17:30:04 -0300

    telas aprimoradas

--- src/main/java/com/bidding/system/frontend/bidding_/frontend/controller/EditalController.java
+++ src/main/java/com/bidding/system/frontend/bidding_/frontend/controller/EditalController.java
@@ -8,14 +8,11 @@ package com.bidding.system.frontend.bidding_.frontend.controller;
 import com.bidding.system.frontend.bidding_.frontend.model.EditalBean;
 import com.bidding.system.frontend.bidding_.frontend.service.AuthService;
 import jakarta.servlet.http.HttpSession;
-import java.util.Calendar;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
--- src/main/java/com/bidding/system/frontend/bidding_/frontend/service/AuthService.java
+++ src/main/java/com/bidding/system/frontend/bidding_/frontend/service/AuthService.java
@@ -9,13 +9,8 @@ import com.bidding.system.frontend.bidding_.frontend.model.UserBean;
 import com.bidding.system.frontend.bidding_.frontend.model.UserLogarBean;
 import java.util.Arrays;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 
 /**
  *
